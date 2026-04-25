d34e765168fecd3a4ea51c6f4a29e5bae468a873
rafaelachein <rafaelachein@gmail.com>
2026-04-25 12:53:33 -0300

    Add event listeners to prevent default actions
    Prevent default actions for context menu, text selection, and drag events.

--- script.js
+++ script.js
@@ -480,6 +480,18 @@ downButton.addEventListener("pointerleave", event => {
   stopDownHold();
 });
 
+document.addEventListener("contextmenu", event => {
+  event.preventDefault();
+});
+
+document.addEventListener("selectstart", event => {
+  event.preventDefault();
+});
+
+document.addEventListener("dragstart", event => {
+  event.preventDefault();
+});
+
 playerReset();
 updateScore();
 updateLevel();
